58d3305c30e3298249d57bfd018c078fb7a5d93e
MarcusHickel <marcus.hickel@gmail.com>
2022-10-22 17:19:27 +1100

    Added Rotation test images
    ROTE*.jpg

--- Testimages/Renaming Folder/RenameScript.m
+++ Testimages/Renaming Folder/RenameScript.m
@@ -3,6 +3,6 @@ files = dir('*.jpg')
 for id = 1:length(files)
     % Get the file name 
     [~, f,ext] = fileparts(files(id).name);
-    rename = strcat('ASY',num2str(id),ext) ;
+    rename = strcat('ROTE',num2str(id),ext) ;
     movefile(files(id).name, rename); 
 end
