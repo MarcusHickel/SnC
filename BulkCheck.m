6f73012674998a452976f14b83a8ddf5eca48a97
MarcusHickel <marcus.hickel@gmail.com>
2022-10-22 17:18:50 +1100

    Added figure creation to BulkCheck

--- BulkCheck.m
+++ BulkCheck.m
@@ -1,7 +1,7 @@
 clear all
 close all
 
-Prefix = 'ASY';
+Prefix = 'ROTE';
 
 j = length(dir(['Testimages/', Prefix, '*.jpg']));
 
@@ -12,7 +12,21 @@ for i = 1:j
     h(i) = subplot(ceil(sqrt(j)),ceil(sqrt(j)),i);
     imageName = strcat(Prefix, num2str(i), '.jpg');
     try
-    BoxFind(imageName);
+    [roi, Corners, Reddiff, Bluediff]= BoxFind(imageName,0);
+    [thetaRecovered, scaleRecovered] = RotationDetect(imageName);
+    
+    I = imread(imageName);
+
+    % Add Blue Lines
+    J = insertShape(I,'Line',[[Corners(1,:)] [Corners(2,:)]],'LineWidth',2,'Color','blue');
+    J = insertShape(J,'Line',[[Corners(3,:)] [Corners(4,:)]],'LineWidth',2,'Color','blue');
+    
+    % Add Red Lines
+    J = insertShape(J,'Line',[[Corners(1,:)] [Corners(3,:)]],'LineWidth',2,'Color','red');
+    J = insertShape(J,'Line',[[Corners(2,:)] [Corners(4,:)]],'LineWidth',2,'Color','red');
+
+    imshow(J);
+    title(sprintf('BlueDiff %4.2f RedDiff %4.2f Rotation %4.2f' ,Bluediff, Reddiff, thetaRecovered));
     catch
     fprintf('Failed on number %d \n', i)
     end
