70db2e0cbfb8bbb31914c6fa0989333330255572
KyTones <jusfoshenanigans@outlook.com>
2022-10-24 01:35:46 +1100

    New Test Files added to do with GUI updates. Further changes are to be added

---
+++ Burner Codes/Test.m
@@ -0,0 +1,35 @@
+%% Question 1
+clear all
+close all
+% rosinit
+sub = rossubscriber('/usb_cam/image_raw');
+[pub,msg] = rospublisher('/usb_cam/image_raw')
+
+[scanData,~,~] = receive(sub,100000000);
+[scanData2,status,statustext] = receive(sub,100000);
+
+original = rgb2gray (scanData.readImage);
+distorted = rgb2gray (scanData2.readImage);
+
+ptsOriginal = detectSURFFeatures(original);
+ptsDistorted = detectSURFFeatures(distorted);
+[featuresOriginal, validPtsOriginal] = extractFeatures(original,ptsOriginal);
+[featuresDistorted, validPtsDistorted] = extractFeatures(distorted,ptsDistorted);
+
+indexPairs = matchFeatures(featuresOriginal,featuresDistorted);
+matchedOriginal = validPtsOriginal(indexPairs(:,1));
+matchedDistorted = validPtsDistorted(indexPairs(:,2));
+
+figure;
+showMatchedFeatures(original,distorted,matchedOriginal,matchedDistorted,'montage');
+
+imshow(scanData.readImage);
+imshow(scanData2.readImage);
+% 
+% 
+% msg = scanData;
+% send(pub,msg);
+%msg = sub.LatestMessage.Data;
+% img = rosReadImage(msg);    
+% imshow(msg);
+% rosshutdown
